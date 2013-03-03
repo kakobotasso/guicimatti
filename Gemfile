7fa8d72fff03e91b230d1efb689c30465516c0be
Kako Botasso <kakodev@gmail.com>
2013-03-03 15:23:13 -0300

    Starting

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+gem 'rails'
+gem 'mysql2'
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'therubyracer', :platforms => :ruby
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+gem 'ckeditor'
+gem 'will_paginate'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guicimatti::Application.config.secret_token = 'a42d0ee78cedcab7842b82e1d14b7a171a8732fbaf31bd87b8a6e18dd9b0baabfe17f3fb4f1fe80bf98bdfd920c592199d953f75f9a7cee3121bd7866f6b68c4'
